2b7e2516e202a7079b0cefb285d1a05ec3bb1069
atong <lt1550919167@163.com>
2022-03-24 11:27:58 +0800

    initial commit

---
+++ src/main/java/cn/atong/leek/spring/beans/factory/BeanFactory.java
@@ -0,0 +1,10 @@
+package cn.atong.leek.spring.beans.factory;
+
+/**
+ * @program: leek-spring
+ * @description: Spring 顶级接口
+ * @author: atong
+ * @create: 2022-03-24 11:24
+ */
+public interface BeanFactory {
+}
