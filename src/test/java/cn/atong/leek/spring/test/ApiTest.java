fb2a9f525229bc0df8fa0291a4d39a1f88be24b1
atong <lt1550919167@163.com>
2022-03-24 15:26:07 +0800

    feat: pom add junit dependency, add Test

---
+++ src/test/java/cn/atong/leek/spring/test/ApiTest.java
@@ -0,0 +1,29 @@
+package cn.atong.leek.spring.test;
+
+import cn.atong.leek.spring.beans.factory.config.BeanDefinition;
+import cn.atong.leek.spring.beans.factory.support.DefaultListableBeanFactory;
+import cn.atong.leek.spring.test.beans.UserService;
+import org.junit.Test;
+
+/**
+ * @program: leek-spring
+ * @description:
+ * @author: atong
+ * @create: 2022-03-24 15:01
+ */
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactory() {
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2.注册 bean
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.第一次获取 bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+}
