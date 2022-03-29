e704e8540a0c4e8c9b23047aae213599f72f5032
atong <lt1550919167@163.com>
2022-03-29 17:12:11 +0800

    feat: version9.0 BeanDefinition中添加 初始化方法名/销毁方法名 Field

--- src/main/java/cn/atong/leek/spring/beans/factory/config/BeanDefinition.java
+++ src/main/java/cn/atong/leek/spring/beans/factory/config/BeanDefinition.java
@@ -15,6 +15,10 @@ public class BeanDefinition {
 
     private PropertyValues propertyValues;
 
+    private String initMethodName;
+
+    private String destroyMethodName;
+
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
         this.propertyValues = new PropertyValues();
@@ -39,5 +43,21 @@ public class BeanDefinition {
 
     public void setPropertyValues(PropertyValues propertyValues) {
         this.propertyValues = propertyValues;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public String getDestroyMethodName() {
+        return destroyMethodName;
+    }
+
+    public void setDestroyMethodName(String destroyMethodName) {
+        this.destroyMethodName = destroyMethodName;
     }
 }
